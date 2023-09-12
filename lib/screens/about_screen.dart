5736dc04a7956c8e52da9d149b63f2842deb555d
marek-jakub <marekjakub@hotmail.co.uk>
2023-09-12 19:56:12 +0200

    First commit.

---
+++ lib/screens/about_screen.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+//import 'package:provider/provider.dart';
+
+class AboutScreen extends StatelessWidget {
+  const AboutScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('About'),
+        centerTitle: true,
+      ),
+      body: const Padding(
+        padding: EdgeInsets.all(8.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          mainAxisSize: MainAxisSize.min,
+          children: <Widget>[
+            Text('search_screen'),
+          ],
+        ),
+      ),
+    );
+  }
+}
