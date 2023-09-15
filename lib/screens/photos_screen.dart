b4ee7a50b9e123bba362a6ecbf6d3489b03f9337
marek-jakub <marekjakub@hotmail.co.uk>
2023-09-15 20:28:29 +0200

    Add nav buttons.

--- lib/screens/photos_screen.dart
+++ lib/screens/photos_screen.dart
@@ -9,15 +9,45 @@ class PhotosScreen extends StatelessWidget {
     return Scaffold(
       appBar: AppBar(
         title: const Text('Photos'),
+        automaticallyImplyLeading: false,
         centerTitle: true,
       ),
-      body: const Padding(
+      body: Padding(
         padding: EdgeInsets.all(8.0),
         child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          mainAxisSize: MainAxisSize.min,
           children: <Widget>[
-            Text('Photos_screen'),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Padding(
+                  padding: const EdgeInsets.fromLTRB(5.0, 2.0, 2.0, 2.0),
+                  child: ElevatedButton(
+                    onPressed: () {
+                      Navigator.pushNamed(context, '/welcome');
+                    },
+                    child: const Text('Home'),
+                  ),
+                ),
+                Padding(
+                  padding: const EdgeInsets.fromLTRB(2.0, 2.0, 2.0, 2.0),
+                  child: ElevatedButton(
+                    onPressed: () {
+                      Navigator.pushNamed(context, '/articles');
+                    },
+                    child: const Text('Articles'),
+                  ),
+                ),
+                Padding(
+                  padding: const EdgeInsets.fromLTRB(2.0, 2.0, 2.0, 2.0),
+                  child: ElevatedButton(
+                    onPressed: () {
+                      Navigator.pushNamed(context, '/about');
+                    },
+                    child: const Text('About'),
+                  ),
+                ),
+              ],
+            ),
           ],
         ),
       ),
