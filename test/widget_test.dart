ff5d1a46a509f2b180a1d5f8accee9067cdd78c1
marek-jakub <marekjakub@hotmail.co.uk>
2023-09-16 20:29:28 +0200

    Change class name.

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -13,7 +13,7 @@ import 'package:personalme/main.dart';
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
     // Build our app and trigger a frame.
-    await tester.pumpWidget(const JakubPersonal());
+    await tester.pumpWidget(const MarkPersonal());
 
     // Verify that our counter starts at 0.
     expect(find.text('0'), findsOneWidget);
