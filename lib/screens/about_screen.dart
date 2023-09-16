f8f51a77bf2cc407307f007b4174b9b1a47aeab9
marek-jakub <marekjakub@hotmail.co.uk>
2023-09-16 20:27:36 +0200

    Add dark mode switch.

--- lib/screens/about_screen.dart
+++ lib/screens/about_screen.dart
@@ -1,56 +1,75 @@
 import 'package:flutter/material.dart';
-//import 'package:provider/provider.dart';
+import 'package:provider/provider.dart';
 
-class AboutScreen extends StatelessWidget {
+import '../models/models.dart';
+
+class AboutScreen extends StatefulWidget {
   const AboutScreen({Key? key}) : super(key: key);
 
   @override
+  State<AboutScreen> createState() => _AboutScreenState();
+}
+
+class _AboutScreenState extends State<AboutScreen> {
+  @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text('About'),
-        automaticallyImplyLeading: false,
-        centerTitle: true,
-      ),
-      body: Padding(
-        padding: EdgeInsets.all(8.0),
-        child: Column(
-          children: <Widget>[
-            Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                Padding(
-                  padding: const EdgeInsets.fromLTRB(5.0, 2.0, 2.0, 2.0),
-                  child: ElevatedButton(
-                    onPressed: () {
-                      Navigator.pushNamed(context, '/welcome');
-                    },
-                    child: const Text('Home'),
-                  ),
-                ),
-                Padding(
-                  padding: const EdgeInsets.fromLTRB(2.0, 2.0, 2.0, 2.0),
-                  child: ElevatedButton(
-                    onPressed: () {
-                      Navigator.pushNamed(context, '/articles');
-                    },
-                    child: const Text('Articles'),
-                  ),
-                ),
-                Padding(
-                  padding: const EdgeInsets.fromLTRB(2.0, 2.0, 2.0, 2.0),
-                  child: ElevatedButton(
-                    onPressed: () {
-                      Navigator.pushNamed(context, '/photos');
-                    },
-                    child: const Text('Photo of the Month'),
-                  ),
-                ),
-              ],
-            ),
+    return Consumer<StateManager>(builder: (context, stateManager, child) {
+      return Scaffold(
+        appBar: AppBar(
+          title: const Text('Marek Jakub: About'),
+          automaticallyImplyLeading: false,
+          centerTitle: true,
+          actions: [
+            Padding(
+              padding: const EdgeInsets.fromLTRB(0, 0, 20, 0),
+              child: Switch(
+                  value: stateManager.darkMode,
+                  onChanged: (value) {
+                    stateManager.setDarkMode = value;
+                  }),
+            )
           ],
         ),
-      ),
-    );
+        body: Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: Column(
+            children: <Widget>[
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Padding(
+                    padding: const EdgeInsets.fromLTRB(5.0, 2.0, 2.0, 2.0),
+                    child: ElevatedButton(
+                      onPressed: () {
+                        Navigator.pushNamed(context, '/welcome');
+                      },
+                      child: const Text('Home'),
+                    ),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.fromLTRB(2.0, 2.0, 2.0, 2.0),
+                    child: ElevatedButton(
+                      onPressed: () {
+                        Navigator.pushNamed(context, '/articles');
+                      },
+                      child: const Text('Articles'),
+                    ),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.fromLTRB(2.0, 2.0, 2.0, 2.0),
+                    child: ElevatedButton(
+                      onPressed: () {
+                        Navigator.pushNamed(context, '/photos');
+                      },
+                      child: const Text('Photo of the Month'),
+                    ),
+                  ),
+                ],
+              ),
+            ],
+          ),
+        ),
+      );
+    });
   }
 }
