130af4ee1515805dcdd5741872ecc121ee6f2b99
marek-jakub <marekjakub@hotmail.co.uk>
2023-09-22 20:45:23 +0200

    First commit.

--- lib/widgets/custom_connect.dart
+++ lib/widgets/custom_connect.dart
@@ -1,1 +1,24 @@
-// TODO: implement bottom nav panel.
+import 'package:flutter/material.dart';
+import 'package:sizer/sizer.dart';
+
+/// A text widget in a sized box.
+class CustomConnectText extends StatelessWidget {
+  const CustomConnectText({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: EdgeInsets.fromLTRB(2.w, 10, 2.w, 25),
+      child: Container(
+        alignment: Alignment.bottomCenter,
+        width: 80.w,
+        child: const Text(
+          style: TextStyle(fontSize: 17),
+          'email',
+        ),
+      ),
+    );
+  }
+}
