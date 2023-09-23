0f080adee7e30bd5c305828c1cb0fdbc4b716a25
marek-jakub <marekjakub@hotmail.co.uk>
2023-09-23 21:25:54 +0200

    Add media query capability.

--- lib/screens/welcome_screen.dart
+++ lib/screens/welcome_screen.dart
@@ -15,6 +15,8 @@ class WelcomeScreen extends StatefulWidget {
 class _WelcomeScreenState extends State<WelcomeScreen> {
   @override
   Widget build(BuildContext context) {
+    final double screenWidth = MediaQuery.of(context).size.width;
+
     return Consumer<StateManager>(
       builder: (context, stateManager, child) {
         return Scaffold(
@@ -35,50 +37,66 @@ class _WelcomeScreenState extends State<WelcomeScreen> {
                   child: Icon(Icons.dark_mode)),
             ],
           ),
-          body: Padding(
-            padding: const EdgeInsets.all(8.0),
-            child: Column(
-              children: <Widget>[
+          body: screenWidth < 700
+              ? smallScreenLayout()
+              : screenWidth < 1200
+                  ? mediumScreenLayout()
+                  : largeScreenLayout(),
+        );
+      },
+    );
+  }
+
+  Widget smallScreenLayout() {
+    return Padding(
+      padding: const EdgeInsets.all(8.0),
+      child: Column(
+        children: <Widget>[
+          Padding(
+            padding: const EdgeInsets.fromLTRB(0, 10, 0, 25),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
                 Padding(
-                  padding: const EdgeInsets.fromLTRB(0, 10, 0, 25),
-                  child: Row(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: [
-                      Padding(
-                        padding: const EdgeInsets.fromLTRB(2.0, 2.0, 2.0, 2.0),
-                        child: ElevatedButton(
-                          onPressed: () {
-                            Navigator.pushNamed(context, '/articles');
-                          },
-                          child: const Text('Articles'),
-                        ),
-                      ),
-                      Padding(
-                        padding: const EdgeInsets.fromLTRB(2.0, 2.0, 2.0, 2.0),
-                        child: ElevatedButton(
-                          onPressed: () {
-                            Navigator.pushNamed(context, '/photos');
-                          },
-                          child: const Text('Photo of the Month'),
-                        ),
-                      ),
-                      Padding(
-                        padding: const EdgeInsets.fromLTRB(2.0, 2.0, 2.0, 2.0),
-                        child: ElevatedButton(
-                          onPressed: () {
-                            Navigator.pushNamed(context, '/about');
-                          },
-                          child: const Text('About'),
-                        ),
-                      ),
-                    ],
+                  padding: const EdgeInsets.fromLTRB(2.0, 2.0, 2.0, 2.0),
+                  child: ElevatedButton(
+                    onPressed: () {
+                      Navigator.pushNamed(context, '/articles');
+                    },
+                    child: const Text('Articles'),
+                  ),
+                ),
+                Padding(
+                  padding: const EdgeInsets.fromLTRB(2.0, 2.0, 2.0, 2.0),
+                  child: ElevatedButton(
+                    onPressed: () {
+                      Navigator.pushNamed(context, '/photos');
+                    },
+                    child: const Text('Photo of the Month'),
+                  ),
+                ),
+                Padding(
+                  padding: const EdgeInsets.fromLTRB(2.0, 2.0, 2.0, 2.0),
+                  child: ElevatedButton(
+                    onPressed: () {
+                      Navigator.pushNamed(context, '/about');
+                    },
+                    child: const Text('About'),
                   ),
                 ),
               ],
             ),
           ),
-        );
-      },
+        ],
+      ),
     );
+  }
+
+  Widget mediumScreenLayout() {
+    return const Text('medium screen layout');
+  }
+
+  Widget largeScreenLayout() {
+    return const Text('large screen layout');
   }
 }
