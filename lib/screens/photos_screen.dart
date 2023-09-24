99ac928fac3435a48c4d4890aca2307fa6fe1844
marek-jakub <marekjakub@hotmail.co.uk>
2023-09-24 19:15:46 +0200

    Implement body in container and scroll view.

--- lib/screens/photos_screen.dart
+++ lib/screens/photos_screen.dart
@@ -35,11 +35,16 @@ class _PhotosScreenState extends State<PhotosScreen> {
                 child: Icon(Icons.dark_mode)),
           ],
         ),
-        body: screenWidth < 700
-            ? smallScreenLayout()
-            : screenWidth < 1200
-                ? mediumScreenLayout()
-                : largeScreenLayout(),
+        body: Container(
+          alignment: Alignment.topCenter,
+          child: SingleChildScrollView(
+            child: screenWidth < 700
+                ? smallScreenLayout()
+                : screenWidth < 1200
+                    ? mediumScreenLayout()
+                    : largeScreenLayout(),
+          ),
+        ),
       );
     });
   }
