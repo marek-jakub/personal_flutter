7129d154f5ea1149db8447cdcc3c8bcfc255e448
marek-jakub <marekjakub@hotmail.co.uk>
2023-09-24 18:20:01 +0200

    Replace nav buttons with custom nav.

--- lib/screens/photos_screen.dart
+++ lib/screens/photos_screen.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 
 import '../models/models.dart';
+import '../widgets/widgets.dart';
 
 class PhotosScreen extends StatefulWidget {
   const PhotosScreen({Key? key}) : super(key: key);
@@ -44,55 +45,38 @@ class _PhotosScreenState extends State<PhotosScreen> {
   }
 
   Widget smallScreenLayout() {
-    return Padding(
-      padding: const EdgeInsets.all(8.0),
+    return const Padding(
+      padding: EdgeInsets.all(8.0),
       child: Column(
         children: <Widget>[
-          Padding(
-            padding: const EdgeInsets.fromLTRB(0, 10, 0, 25),
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                Padding(
-                  padding: const EdgeInsets.fromLTRB(5.0, 2.0, 2.0, 2.0),
-                  child: ElevatedButton(
-                    onPressed: () {
-                      Navigator.pushNamed(context, '/welcome');
-                    },
-                    child: const Text('Home'),
-                  ),
-                ),
-                Padding(
-                  padding: const EdgeInsets.fromLTRB(2.0, 2.0, 2.0, 2.0),
-                  child: ElevatedButton(
-                    onPressed: () {
-                      Navigator.pushNamed(context, '/articles');
-                    },
-                    child: const Text('Articles'),
-                  ),
-                ),
-                Padding(
-                  padding: const EdgeInsets.fromLTRB(2.0, 2.0, 2.0, 2.0),
-                  child: ElevatedButton(
-                    onPressed: () {
-                      Navigator.pushNamed(context, '/about');
-                    },
-                    child: const Text('About'),
-                  ),
-                ),
-              ],
-            ),
-          ),
+          CustomNavButtons(),
+          Text('small screen layout'),
         ],
       ),
     );
   }
 
   Widget mediumScreenLayout() {
-    return const Text('medium screen layout');
+    return const Padding(
+      padding: EdgeInsets.all(8.0),
+      child: Column(
+        children: <Widget>[
+          CustomNavButtons(),
+          Text('medium screen layout'),
+        ],
+      ),
+    );
   }
 
   Widget largeScreenLayout() {
-    return const Text('large screen layout');
+    return const Padding(
+      padding: EdgeInsets.all(8.0),
+      child: Column(
+        children: <Widget>[
+          CustomNavButtons(),
+          Text('large screen layout'),
+        ],
+      ),
+    );
   }
 }
