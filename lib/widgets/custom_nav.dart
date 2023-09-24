8e1103da789fd8a4cf2722bf70058151e1fa7e32
marek-jakub <marekjakub@hotmail.co.uk>
2023-09-24 19:14:19 +0200

    Implement media query for screen sizes.

--- lib/widgets/custom_nav.dart
+++ lib/widgets/custom_nav.dart
@@ -8,49 +8,102 @@ class CustomNavButtons extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Padding(
-      padding: const EdgeInsets.fromLTRB(0, 10, 0, 25),
-      child: Row(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          Padding(
-            padding: const EdgeInsets.fromLTRB(5.0, 2.0, 2.0, 2.0),
-            child: ElevatedButton(
-              onPressed: () {
-                Navigator.pushNamed(context, '/welcome');
-              },
-              child: const Text('Home'),
-            ),
-          ),
-          Padding(
-            padding: const EdgeInsets.fromLTRB(2.0, 2.0, 2.0, 2.0),
-            child: ElevatedButton(
-              onPressed: () {
-                Navigator.pushNamed(context, '/articles');
-              },
-              child: const Text('Articles'),
-            ),
-          ),
-          Padding(
-            padding: const EdgeInsets.fromLTRB(2.0, 2.0, 2.0, 2.0),
-            child: ElevatedButton(
-              onPressed: () {
-                Navigator.pushNamed(context, '/photos');
-              },
-              child: const Text('Photo of the Month'),
-            ),
-          ),
-          Padding(
-            padding: const EdgeInsets.fromLTRB(2.0, 2.0, 2.0, 2.0),
-            child: ElevatedButton(
-              onPressed: () {
-                Navigator.pushNamed(context, '/about');
-              },
-              child: const Text('About'),
-            ),
-          ),
-        ],
-      ),
-    );
+    final double screenWidth = MediaQuery.of(context).size.width;
+    return screenWidth < 700
+        ? smallScreenLayout(context)
+        : mediumAndLargeScreenLayout(context);
   }
+}
+
+Widget smallScreenLayout(BuildContext context) {
+  return Padding(
+    padding: const EdgeInsets.fromLTRB(0, 0, 0, 25),
+    child: Column(
+      children: [
+        Padding(
+          padding: const EdgeInsets.fromLTRB(2.0, 2.0, 2.0, 2.0),
+          child: ElevatedButton(
+            onPressed: () {
+              Navigator.pushNamed(context, '/welcome');
+            },
+            child: const Text('Home'),
+          ),
+        ),
+        Padding(
+          padding: const EdgeInsets.fromLTRB(2.0, 2.0, 2.0, 2.0),
+          child: ElevatedButton(
+            onPressed: () {
+              Navigator.pushNamed(context, '/articles');
+            },
+            child: const Text('Articles'),
+          ),
+        ),
+        Padding(
+          padding: const EdgeInsets.fromLTRB(2.0, 2.0, 2.0, 2.0),
+          child: ElevatedButton(
+            onPressed: () {
+              Navigator.pushNamed(context, '/photos');
+            },
+            child: const Text('Photo of the Month'),
+          ),
+        ),
+        Padding(
+          padding: const EdgeInsets.fromLTRB(2.0, 2.0, 2.0, 2.0),
+          child: ElevatedButton(
+            onPressed: () {
+              Navigator.pushNamed(context, '/about');
+            },
+            child: const Text('About'),
+          ),
+        ),
+      ],
+    ),
+  );
+}
+
+Widget mediumAndLargeScreenLayout(BuildContext context) {
+  return Padding(
+    padding: const EdgeInsets.fromLTRB(0, 0, 0, 25),
+    child: Row(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        Padding(
+          padding: const EdgeInsets.fromLTRB(2.0, 2.0, 2.0, 2.0),
+          child: ElevatedButton(
+            onPressed: () {
+              Navigator.pushNamed(context, '/welcome');
+            },
+            child: const Text('Home'),
+          ),
+        ),
+        Padding(
+          padding: const EdgeInsets.fromLTRB(2.0, 2.0, 2.0, 2.0),
+          child: ElevatedButton(
+            onPressed: () {
+              Navigator.pushNamed(context, '/articles');
+            },
+            child: const Text('Articles'),
+          ),
+        ),
+        Padding(
+          padding: const EdgeInsets.fromLTRB(2.0, 2.0, 2.0, 2.0),
+          child: ElevatedButton(
+            onPressed: () {
+              Navigator.pushNamed(context, '/photos');
+            },
+            child: const Text('Photo of the Month'),
+          ),
+        ),
+        Padding(
+          padding: const EdgeInsets.fromLTRB(2.0, 2.0, 2.0, 2.0),
+          child: ElevatedButton(
+            onPressed: () {
+              Navigator.pushNamed(context, '/about');
+            },
+            child: const Text('About'),
+          ),
+        ),
+      ],
+    ),
+  );
 }
