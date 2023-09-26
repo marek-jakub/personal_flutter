ecdc73e92f51b9434b46e02af6b9ded2e884ada2
marek-jakub <marekjakub@hotmail.co.uk>
2023-09-26 18:40:16 +0200

    Add font size parameter.

--- lib/widgets/custom_year_widget.dart
+++ lib/widgets/custom_year_widget.dart
@@ -1,25 +1,27 @@
 import 'package:flutter/material.dart';
-import 'package:sizer/sizer.dart';
+//import 'package:sizer/sizer.dart';
 
 /// A text widget in a sized box.
 class CustomDateText extends StatelessWidget {
   const CustomDateText({
     Key? key,
     required String date,
+    required double fontSize,
   })  : _date = date,
+        _fontSize = fontSize,
         super(key: key);
 
   final String _date;
+  final double _fontSize;
 
   @override
   Widget build(BuildContext context) {
     return Padding(
-      padding: EdgeInsets.fromLTRB(0, 25, 0, 25),
-      child: SizedBox(
-        width: 80.w,
+      padding: const EdgeInsets.fromLTRB(0, 25, 0, 25),
+      child: Center(
         child: Text(
-          style: TextStyle(fontSize: 17),
           _date,
+          style: TextStyle(fontSize: _fontSize),
         ),
       ),
     );
