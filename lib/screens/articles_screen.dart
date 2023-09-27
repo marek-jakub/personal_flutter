5a9ee170afb47a59a3b5d15491191dd4864e7ab7
marek-jakub <marekjakub@hotmail.co.uk>
2023-09-27 18:56:31 +0200

    Comment out unneeded import.

--- lib/screens/articles_screen.dart
+++ lib/screens/articles_screen.dart
@@ -1,4 +1,4 @@
-import 'package:flutter/gestures.dart';
+//import 'package:flutter/gestures.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:sizer/sizer.dart';
