e4c449c3879a80a3ad326f68d1d10b02e210cebb
marek-jakub <marekjakub@hotmail.co.uk>
2023-10-05 20:27:51 +0200

    Add custom navigation export.

--- lib/widgets/widgets.dart
+++ lib/widgets/widgets.dart
@@ -1,4 +1,5 @@
 export 'custom_clickable_article_text.dart';
 export 'custom_connect.dart';
-export 'custom_nav.dart';
+//export 'custom_nav.dart';
+export 'custom_navigation.dart';
 export 'custom_year_widget.dart';
