6a2c31904675f07c3bbf0b9932128c15d27bfac4
marek-jakub <marekjakub@hotmail.co.uk>
2023-10-05 20:28:33 +0200

    Commenting out the widget.

--- lib/widgets/custom_nav.dart
+++ lib/widgets/custom_nav.dart
@@ -1,91 +1,91 @@
-import 'package:flutter/material.dart';
+// import 'package:flutter/material.dart';
 
-/// Website navigation buttons.
-class CustomNavButtons extends StatelessWidget {
-  const CustomNavButtons({
-    Key? key,
-  }) : super(key: key);
+// /// Website navigation buttons.
+// class CustomNavButtons extends StatelessWidget {
+//   const CustomNavButtons({
+//     Key? key,
+//   }) : super(key: key);
 
-  @override
-  Widget build(BuildContext context) {
-    final double screenWidth = MediaQuery.of(context).size.width;
-    return screenWidth < 700
-        ? smallScreenLayout(context)
-        : mediumAndLargeScreenLayout(context);
-  }
-}
+//   @override
+//   Widget build(BuildContext context) {
+//     final double screenWidth = MediaQuery.of(context).size.width;
+//     return screenWidth < 700
+//         ? smallScreenLayout(context)
+//         : mediumAndLargeScreenLayout(context);
+//   }
+// }
 
-Widget smallScreenLayout(BuildContext context) {
-  return Padding(
-    padding: const EdgeInsets.fromLTRB(0, 0, 0, 25),
-    child: Column(
-      children: [
-        Padding(
-          padding: const EdgeInsets.fromLTRB(2.0, 2.0, 2.0, 2.0),
-          child: ElevatedButton(
-            onPressed: () {
-              Navigator.pushNamed(context, '/welcome');
-            },
-            child: const Text('Home'),
-          ),
-        ),
-        Padding(
-          padding: const EdgeInsets.fromLTRB(2.0, 2.0, 2.0, 2.0),
-          child: ElevatedButton(
-            onPressed: () {
-              Navigator.pushNamed(context, '/articles');
-            },
-            child: const Text('Articles'),
-          ),
-        ),
-        Padding(
-          padding: const EdgeInsets.fromLTRB(2.0, 2.0, 2.0, 2.0),
-          child: ElevatedButton(
-            onPressed: () {
-              Navigator.pushNamed(context, '/about');
-            },
-            child: const Text('About'),
-          ),
-        ),
-      ],
-    ),
-  );
-}
+// Widget smallScreenLayout(BuildContext context) {
+//   return Padding(
+//     padding: const EdgeInsets.fromLTRB(0, 0, 0, 25),
+//     child: Column(
+//       children: [
+//         Padding(
+//           padding: const EdgeInsets.fromLTRB(2.0, 2.0, 2.0, 2.0),
+//           child: ElevatedButton(
+//             onPressed: () {
+//               Navigator.pushNamed(context, '/welcome');
+//             },
+//             child: const Text('Home'),
+//           ),
+//         ),
+//         Padding(
+//           padding: const EdgeInsets.fromLTRB(2.0, 2.0, 2.0, 2.0),
+//           child: ElevatedButton(
+//             onPressed: () {
+//               Navigator.pushNamed(context, '/articles');
+//             },
+//             child: const Text('Articles'),
+//           ),
+//         ),
+//         Padding(
+//           padding: const EdgeInsets.fromLTRB(2.0, 2.0, 2.0, 2.0),
+//           child: ElevatedButton(
+//             onPressed: () {
+//               Navigator.pushNamed(context, '/about');
+//             },
+//             child: const Text('About'),
+//           ),
+//         ),
+//       ],
+//     ),
+//   );
+// }
 
-Widget mediumAndLargeScreenLayout(BuildContext context) {
-  return Padding(
-    padding: const EdgeInsets.fromLTRB(0, 0, 0, 25),
-    child: Row(
-      mainAxisAlignment: MainAxisAlignment.center,
-      children: [
-        Padding(
-          padding: const EdgeInsets.fromLTRB(2.0, 2.0, 2.0, 2.0),
-          child: ElevatedButton(
-            onPressed: () {
-              Navigator.pushNamed(context, '/welcome');
-            },
-            child: const Text('Home'),
-          ),
-        ),
-        Padding(
-          padding: const EdgeInsets.fromLTRB(2.0, 2.0, 2.0, 2.0),
-          child: ElevatedButton(
-            onPressed: () {
-              Navigator.pushNamed(context, '/articles');
-            },
-            child: const Text('Articles'),
-          ),
-        ),
-        Padding(
-          padding: const EdgeInsets.fromLTRB(2.0, 2.0, 2.0, 2.0),
-          child: ElevatedButton(
-            onPressed: () {
-              Navigator.pushNamed(context, '/about');
-            },
-            child: const Text('About'),
-          ),
-        ),
-      ],
-    ),
-  );
-}
+// Widget mediumAndLargeScreenLayout(BuildContext context) {
+//   return Padding(
+//     padding: const EdgeInsets.fromLTRB(0, 0, 0, 25),
+//     child: Row(
+//       mainAxisAlignment: MainAxisAlignment.center,
+//       children: [
+//         Padding(
+//           padding: const EdgeInsets.fromLTRB(2.0, 2.0, 2.0, 2.0),
+//           child: ElevatedButton(
+//             onPressed: () {
+//               Navigator.pushNamed(context, '/welcome');
+//             },
+//             child: const Text('Home'),
+//           ),
+//         ),
+//         Padding(
+//           padding: const EdgeInsets.fromLTRB(2.0, 2.0, 2.0, 2.0),
+//           child: ElevatedButton(
+//             onPressed: () {
+//               Navigator.pushNamed(context, '/articles');
+//             },
+//             child: const Text('Articles'),
+//           ),
+//         ),
+//         Padding(
+//           padding: const EdgeInsets.fromLTRB(2.0, 2.0, 2.0, 2.0),
+//           child: ElevatedButton(
+//             onPressed: () {
+//               Navigator.pushNamed(context, '/about');
+//             },
+//             child: const Text('About'),
+//           ),
+//         ),
+//       ],
+//     ),
+//   );
+// }
