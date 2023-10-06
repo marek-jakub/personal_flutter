6779e1b9fe23961ff91ec7ae94420eebe968060c
marek-jakub <marekjakub@hotmail.co.uk>
2023-10-06 16:29:38 +0200

    Add custom_app_bar export.

--- lib/widgets/widgets.dart
+++ lib/widgets/widgets.dart
@@ -1,5 +1,5 @@
+export 'custom_app_bar.dart';
 export 'custom_clickable_article_text.dart';
-export 'custom_connect.dart';
 //export 'custom_nav.dart';
 export 'custom_navigation.dart';
 export 'custom_year_widget.dart';
