cd69278f48b9584f037703843c62f5ef669ea39e
marek-jakub <marekjakub@hotmail.co.uk>
2023-10-08 18:59:17 +0200

    Add custom mode selection export.

--- lib/widgets/widgets.dart
+++ lib/widgets/widgets.dart
@@ -1,5 +1,6 @@
 export 'custom_app_bar.dart';
 export 'custom_clickable_article_text.dart';
+export 'custom_mode_selection.dart';
 //export 'custom_nav.dart';
 export 'custom_navigation.dart';
 export 'custom_year_widget.dart';
