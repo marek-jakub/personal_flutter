93ff48574711df5c6fccc9761dc6160b032cf170
marek-jakub <marekjakub@hotmail.co.uk>
2023-10-08 19:00:33 +0200

    Remove row and place custom mode selection in flexible.

--- lib/widgets/custom_app_bar.dart
+++ lib/widgets/custom_app_bar.dart
@@ -1,9 +1,9 @@
 import 'package:flutter/material.dart';
-import 'package:provider/provider.dart';
+//import 'package:provider/provider.dart';
 //import 'package:sizer/sizer.dart';
-import 'package:flutter_sizer/flutter_sizer.dart';
+//import 'package:flutter_sizer/flutter_sizer.dart';
 
-import '../models/models.dart';
+//import '../models/models.dart';
 import '../widgets/widgets.dart';
 
 /// Website app bar.
@@ -19,40 +19,30 @@ class CustomAppBar extends StatefulWidget {
 class _CustomAppBarState extends State<CustomAppBar> {
   @override
   Widget build(BuildContext context) {
-    return Consumer<StateManager>(
-      builder: (constext, stateManager, child) {
-        return AppBar(
-          automaticallyImplyLeading: false,
-          title: Row(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              const Flexible(
-                child: Image(
-                  width: 60,
-                  height: 60,
-                  fit: BoxFit.scaleDown,
-                  image: AssetImage('assets/images/02_logo.png'),
-                ),
-              ),
-              Flexible(
-                  child: Row(
-                mainAxisAlignment: MainAxisAlignment.end,
-                children: [
-                  const CustomNavigation(),
-                  const Icon(Icons.light_mode),
-                  Switch(
-                    value: stateManager.darkMode,
-                    onChanged: (value) {
-                      stateManager.setDarkMode = value;
-                    },
-                  ),
-                  const Icon(Icons.dark_mode),
-                ],
-              ))
-            ],
+    return AppBar(
+      automaticallyImplyLeading: false,
+      title: const Row(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Flexible(
+            fit: FlexFit.tight,
+            child: Image(
+              width: 60,
+              height: 60,
+              fit: BoxFit.scaleDown,
+              image: AssetImage('assets/images/02_logo.png'),
+            ),
           ),
-        );
-      },
+          Flexible(
+            fit: FlexFit.loose,
+            child: CustomNavigation(),
+          ),
+          Flexible(
+            fit: FlexFit.loose,
+            child: CustomModeSelection(),
+          ),
+        ],
+      ),
     );
   }
 }
