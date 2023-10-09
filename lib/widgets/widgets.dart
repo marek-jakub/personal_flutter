d1ef08f3c188ff916583eef1a6f910dd9964c30e
marek-jakub <marekjakub@hotmail.co.uk>
2023-10-09 16:58:23 +0200

    Add custom text invite export.

--- lib/widgets/widgets.dart
+++ lib/widgets/widgets.dart
@@ -3,4 +3,5 @@ export 'custom_clickable_article_text.dart';
 export 'custom_mode_selection.dart';
 //export 'custom_nav.dart';
 export 'custom_navigation.dart';
+export 'custom_text_invite.dart';
 export 'custom_year_widget.dart';
