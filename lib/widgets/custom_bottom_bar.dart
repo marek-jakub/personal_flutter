58525ea49c607c750f6a5c17fc57407668672824
marek-jakub <marekjakub@hotmail.co.uk>
2023-10-10 17:47:55 +0200

    Initial commit.

---
+++ lib/widgets/custom_bottom_bar.dart
@@ -0,0 +1,34 @@
+import 'package:flutter/material.dart';
+
+/// A BottomBar with copyright and date.
+class CustomBottomBar extends StatelessWidget {
+  const CustomBottomBar({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.fromLTRB(0, 100, 0, 20),
+      child: Container(
+        height: 70,
+        decoration: const BoxDecoration(
+          border: Border(
+            top: BorderSide(
+              color: Colors.grey,
+              width: 2.0,
+            ),
+          ),
+        ),
+        child: const Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text('2023'),
+            Icon(Icons.copyright),
+            Text('Marek Jakub'),
+          ],
+        ),
+      ),
+    );
+  }
+}
