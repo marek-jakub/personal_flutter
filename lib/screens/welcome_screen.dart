90371dc14cb29b6d86e8269dc736f5c3d06cb404
marek-jakub <marekjakub@hotmail.co.uk>
2023-10-10 17:48:27 +0200

    Add small and medium screen implementation.

--- lib/screens/welcome_screen.dart
+++ lib/screens/welcome_screen.dart
@@ -43,99 +43,249 @@ class _WelcomeScreenState extends State<WelcomeScreen> {
   }
 
   Widget smallScreenLayout(TextInvite invite) {
-    return const Padding(
-      padding: EdgeInsets.all(8.0),
+    return SizedBox(
       child: Column(
-        children: <Widget>[
-          Text('small screen layout'),
+        children: [
+          Container(
+            width: 700,
+            alignment: Alignment.center,
+            child: Column(
+              children: [
+                CustomContainer(
+                    width: 400,
+                    height: 400,
+                    date: '01 Sep 2021',
+                    articleName: 'Article name',
+                    articlePath: 'Article path',
+                    articleText: 'Sljksadf asdlfkj sdlfkja sa;dflk sflk j',
+                    titleSize: 14,
+                    fontSize: 12,
+                    boxColor: Colors.yellow),
+                CustomContainer(
+                    width: 400,
+                    height: 400,
+                    date: invite.article5['articleDate'].toString(),
+                    articleName: invite.article5['articleName'].toString(),
+                    articlePath: '/traversal',
+                    articleText: invite.article5['articleText'].toString(),
+                    titleSize: 14,
+                    fontSize: 12,
+                    boxColor: Colors.brown),
+                CustomContainer(
+                    width: 400,
+                    height: 400,
+                    date: invite.article4['articleDate'].toString(),
+                    articleName: invite.article4['articleName'].toString(),
+                    articlePath: '/solvenot',
+                    articleText: invite.article4['articleText'].toString(),
+                    titleSize: 14,
+                    fontSize: 12,
+                    boxColor: Colors.red),
+                CustomContainer(
+                    width: 400,
+                    height: 400,
+                    date: invite.article3['articleDate'].toString(),
+                    articleName: invite.article3['articleName'].toString(),
+                    articlePath: '/hunt',
+                    articleText: invite.article3['articleText'].toString(),
+                    titleSize: 14,
+                    fontSize: 12,
+                    boxColor: Colors.black),
+                CustomContainer(
+                    width: 400,
+                    height: 400,
+                    date: invite.article2['articleDate'].toString(),
+                    articleName: invite.article2['articleName'].toString(),
+                    articlePath: '/bookcase',
+                    articleText: invite.article2['articleText'].toString(),
+                    titleSize: 14,
+                    fontSize: 12,
+                    boxColor: Colors.blue),
+                CustomContainer(
+                    width: 400,
+                    height: 400,
+                    date: invite.article1['articleDate'].toString(),
+                    articleName: invite.article1['articleName'].toString(),
+                    articlePath: '/binarytree',
+                    articleText: invite.article1['articleText'].toString(),
+                    titleSize: 14,
+                    fontSize: 12,
+                    boxColor: Colors.orange),
+              ],
+            ),
+          ),
+          const CustomBottomBar(),
         ],
       ),
     );
   }
 
   Widget mediumScreenLayout(TextInvite invite) {
-    return const Column(
-      children: [
-        Text('medium screen layout'),
-      ],
+    return SizedBox(
+      child: Column(
+        children: [
+          Container(
+            width: 1000,
+            alignment: Alignment.center,
+            child: Column(
+              children: [
+                CustomContainer(
+                    width: 500,
+                    height: 500,
+                    date: '01 Sep 2021',
+                    articleName: 'Article name',
+                    articlePath: 'Article path',
+                    articleText: 'Sljksadf asdlfkj sdlfkja sa;dflk sflk j',
+                    titleSize: 15,
+                    fontSize: 14,
+                    boxColor: Colors.yellow),
+                CustomContainer(
+                    width: 500,
+                    height: 500,
+                    date: invite.article5['articleDate'].toString(),
+                    articleName: invite.article5['articleName'].toString(),
+                    articlePath: '/traversal',
+                    articleText: invite.article5['articleText'].toString(),
+                    titleSize: 15,
+                    fontSize: 14,
+                    boxColor: Colors.brown),
+                CustomContainer(
+                    width: 500,
+                    height: 500,
+                    date: invite.article4['articleDate'].toString(),
+                    articleName: invite.article4['articleName'].toString(),
+                    articlePath: '/solvenot',
+                    articleText: invite.article4['articleText'].toString(),
+                    titleSize: 15,
+                    fontSize: 14,
+                    boxColor: Colors.red),
+                CustomContainer(
+                    width: 500,
+                    height: 500,
+                    date: invite.article3['articleDate'].toString(),
+                    articleName: invite.article3['articleName'].toString(),
+                    articlePath: '/hunt',
+                    articleText: invite.article3['articleText'].toString(),
+                    titleSize: 15,
+                    fontSize: 14,
+                    boxColor: Colors.black),
+                CustomContainer(
+                    width: 500,
+                    height: 500,
+                    date: invite.article2['articleDate'].toString(),
+                    articleName: invite.article2['articleName'].toString(),
+                    articlePath: '/bookcase',
+                    articleText: invite.article2['articleText'].toString(),
+                    titleSize: 15,
+                    fontSize: 14,
+                    boxColor: Colors.blue),
+                CustomContainer(
+                    width: 500,
+                    height: 500,
+                    date: invite.article1['articleDate'].toString(),
+                    articleName: invite.article1['articleName'].toString(),
+                    articlePath: '/binarytree',
+                    articleText: invite.article1['articleText'].toString(),
+                    titleSize: 15,
+                    fontSize: 14,
+                    boxColor: Colors.orange),
+              ],
+            ),
+          ),
+          const CustomBottomBar(),
+        ],
+      ),
     );
   }
 
   Widget largeScreenLayout(TextInvite invite) {
-    return Container(
-      width: 1400,
-      alignment: Alignment.center,
+    return SizedBox(
       child: Column(
         children: [
-          Row(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              CustomContainer(
-                  width: 500,
-                  height: 500,
-                  date: '01 Sep 2021',
-                  articleName: 'Article name',
-                  articlePath: 'Article path',
-                  articleText: 'Sljksadf asdlfkj sdlfkja sa;dflk sflk j',
-                  fontSize: 16,
-                  boxColor: Colors.yellow),
-              CustomContainer(
-                  width: 500,
-                  height: 500,
-                  date: invite.article5['articleDate'].toString(),
-                  articleName: invite.article5['articleName'].toString(),
-                  articlePath: '/traversal',
-                  articleText: invite.article5['articleText'].toString(),
-                  fontSize: 16,
-                  boxColor: Colors.brown),
-            ],
+          Container(
+            width: 1400,
+            alignment: Alignment.center,
+            child: Column(
+              children: [
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    CustomContainer(
+                        width: 500,
+                        height: 500,
+                        date: '01 Sep 2021',
+                        articleName: 'Article name',
+                        articlePath: 'Article path',
+                        articleText: 'Sljksadf asdlfkj sdlfkja sa;dflk sflk j',
+                        titleSize: 16,
+                        fontSize: 14,
+                        boxColor: Colors.yellow),
+                    CustomContainer(
+                        width: 500,
+                        height: 500,
+                        date: invite.article5['articleDate'].toString(),
+                        articleName: invite.article5['articleName'].toString(),
+                        articlePath: '/traversal',
+                        articleText: invite.article5['articleText'].toString(),
+                        titleSize: 16,
+                        fontSize: 14,
+                        boxColor: Colors.brown),
+                  ],
+                ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    CustomContainer(
+                        width: 500,
+                        height: 500,
+                        date: invite.article4['articleDate'].toString(),
+                        articleName: invite.article4['articleName'].toString(),
+                        articlePath: '/solvenot',
+                        articleText: invite.article4['articleText'].toString(),
+                        titleSize: 16,
+                        fontSize: 14,
+                        boxColor: Colors.red),
+                    CustomContainer(
+                        width: 500,
+                        height: 500,
+                        date: invite.article3['articleDate'].toString(),
+                        articleName: invite.article3['articleName'].toString(),
+                        articlePath: '/hunt',
+                        articleText: invite.article3['articleText'].toString(),
+                        titleSize: 16,
+                        fontSize: 14,
+                        boxColor: Colors.black),
+                  ],
+                ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    CustomContainer(
+                        width: 500,
+                        height: 500,
+                        date: invite.article2['articleDate'].toString(),
+                        articleName: invite.article2['articleName'].toString(),
+                        articlePath: '/bookcase',
+                        articleText: invite.article2['articleText'].toString(),
+                        titleSize: 16,
+                        fontSize: 14,
+                        boxColor: Colors.blue),
+                    CustomContainer(
+                        width: 500,
+                        height: 500,
+                        date: invite.article1['articleDate'].toString(),
+                        articleName: invite.article1['articleName'].toString(),
+                        articlePath: '/binarytree',
+                        articleText: invite.article1['articleText'].toString(),
+                        titleSize: 16,
+                        fontSize: 14,
+                        boxColor: Colors.orange),
+                  ],
+                ),
+              ],
+            ),
           ),
-          Row(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              CustomContainer(
-                  width: 500,
-                  height: 500,
-                  date: invite.article4['articleDate'].toString(),
-                  articleName: invite.article4['articleName'].toString(),
-                  articlePath: '/solvenot',
-                  articleText: invite.article4['articleText'].toString(),
-                  fontSize: 16,
-                  boxColor: Colors.red),
-              CustomContainer(
-                  width: 500,
-                  height: 500,
-                  date: invite.article3['articleDate'].toString(),
-                  articleName: invite.article3['articleName'].toString(),
-                  articlePath: '/hunt',
-                  articleText: invite.article3['articleText'].toString(),
-                  fontSize: 16,
-                  boxColor: Colors.black),
-            ],
-          ),
-          Row(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              CustomContainer(
-                  width: 500,
-                  height: 500,
-                  date: invite.article2['articleDate'].toString(),
-                  articleName: invite.article2['articleName'].toString(),
-                  articlePath: '/bookcase',
-                  articleText: invite.article2['articleText'].toString(),
-                  fontSize: 16,
-                  boxColor: Colors.blue),
-              CustomContainer(
-                  width: 500,
-                  height: 500,
-                  date: invite.article1['articleDate'].toString(),
-                  articleName: invite.article1['articleName'].toString(),
-                  articlePath: '/binarytree',
-                  articleText: invite.article1['articleText'].toString(),
-                  fontSize: 16,
-                  boxColor: Colors.orange),
-            ],
-          ),
+          const CustomBottomBar(),
         ],
       ),
     );
