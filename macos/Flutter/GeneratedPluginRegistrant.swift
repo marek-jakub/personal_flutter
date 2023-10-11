9f189877d23538e88de7c2fdce67389469c9caf3
marek-jakub <marekjakub@hotmail.co.uk>
2023-10-11 17:43:29 +0200

    Add video, wakelock plugins.

--- macos/Flutter/GeneratedPluginRegistrant.swift
+++ macos/Flutter/GeneratedPluginRegistrant.swift
@@ -7,8 +7,12 @@ import Foundation
 
 import path_provider_foundation
 import url_launcher_macos
+import video_player_avfoundation
+import wakelock_macos
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   PathProviderPlugin.register(with: registry.registrar(forPlugin: "PathProviderPlugin"))
   UrlLauncherPlugin.register(with: registry.registrar(forPlugin: "UrlLauncherPlugin"))
+  FVPVideoPlayerPlugin.register(with: registry.registrar(forPlugin: "FVPVideoPlayerPlugin"))
+  WakelockMacosPlugin.register(with: registry.registrar(forPlugin: "WakelockMacosPlugin"))
 }
