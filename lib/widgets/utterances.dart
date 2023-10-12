124910935c8907b2f05b35a50ff60b04e57e57ac
marek-jakub <marekjakub@hotmail.co.uk>
2023-10-12 16:46:29 +0200

    Initial commit.

---
+++ lib/widgets/utterances.dart
@@ -0,0 +1,31 @@
+import 'dart:html' as html;
+//import 'dart:js' as js;
+import 'dart:ui_web' as ui;
+
+import 'package:flutter/material.dart';
+
+class UtterancesWidget extends StatelessWidget {
+  const UtterancesWidget({super.key});
+
+  final String viewID = "utterances";
+
+  @override
+  Widget build(BuildContext context) {
+    // ignore: undefined_prefixed_name
+    ui.platformViewRegistry.registerViewFactory(
+      viewID,
+      (int id) => html.IFrameElement()
+        ..width = MediaQuery.of(context).size.width.toString()
+        ..height = MediaQuery.of(context).size.height.toString()
+        ..src = 'assets/utterances.html'
+        ..style.border = 'none',
+    );
+
+    return SizedBox(
+      height: 700,
+      child: HtmlElementView(
+        viewType: viewID,
+      ),
+    );
+  }
+}
