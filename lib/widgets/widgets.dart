adddbfdb490f6d512e48cf4a6554d2d38f28821a
marek-jakub <marekjakub@hotmail.co.uk>
2023-10-13 12:38:57 +0200

    Remove unneeded exports.

--- lib/widgets/widgets.dart
+++ lib/widgets/widgets.dart
@@ -2,7 +2,6 @@ export 'custom_app_bar.dart';
 export 'custom_bottom_bar.dart';
 export 'custom_clickable_article_text.dart';
 export 'custom_mode_selection.dart';
-//export 'custom_nav.dart';
 export 'custom_navigation.dart';
 export 'custom_text_invite.dart';
 export 'custom_year_widget.dart';
