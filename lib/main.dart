4b5662ac5b1978a2ae3474436fb22ba06917b420
marek-jakub <marekjakub@hotmail.co.uk>
2023-11-01 17:54:16 +0100

    Update url strategy.

--- lib/main.dart
+++ lib/main.dart
@@ -2,7 +2,8 @@ import 'dart:ui';
 
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
-import 'package:url_strategy/url_strategy.dart';
+//import 'package:url_strategy/url_strategy.dart';
+import 'package:flutter_web_plugins/url_strategy.dart';
 import 'package:flutter_sizer/flutter_sizer.dart';
 
 import '../models/models.dart';
@@ -11,7 +12,7 @@ import '../router/route_generator.dart';
 import 'personalme_theme.dart';
 
 void main() {
-  setPathUrlStrategy();
+  usePathUrlStrategy();
   runApp(const MarkPersonal());
 }
 
